2067c0fbbade3014e990213ef73eebd840648cd7
Lasse <lassef91@gmail.com>
2023-10-13 23:09:03 +0200

    Secret key

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import Losliste
+import os
 
 app = Flask(__name__)
-app.secret_key = 'HEMMELIG'
+app.secret_key = os.getenv('SECRET_KEY')
 
-Losliste.current_pilotages()
-#Losliste.start()
+#Losliste.current_pilotages()
+Losliste.start()
 
 @app.route("/")
 def index():
@@ -29,9 +30,9 @@ def index():
 
 if __name__ == "__main__":
     #Comment out for local testing
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
-    app.run(debug=True) #Uncomment for local testing
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    #app.run(debug=True, host='0.0.0.0') #Uncomment for local testing
 
 @app.route('/sort', methods=['POST'])
 def sort():
